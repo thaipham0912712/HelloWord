91ce27d02c41f6d53120bca7140f5d79f985c3fa
thaipham0912712 <ngocthaipham1996@gmail.com>
2019-06-09 20:58:29 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class Hello{
-      public static void main(String[] args) {
+      public static void main(String args[]) {
           //print "Hello,World" in the terminal window
             System.out.println("Hello, World");
       }
